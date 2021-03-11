990355827d9cde1b64789c797d79ea62cc7bb598
raihanM95 <mahmud.raihan@yahoo.com>
2021-03-11 16:59:13 +0600

    Initial RepositoryContext

---
+++ OnionArchitecture.Infrastructure.Persistence/Contexts/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Infrastructure.Persistence.Contexts
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
