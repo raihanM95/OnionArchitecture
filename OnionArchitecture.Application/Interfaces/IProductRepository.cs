379aa793f5b21a46e75537048cab469037b78c40
raihanM95 <mahmud.raihan@yahoo.com>
2021-03-11 15:52:00 +0600

    Initial IGeneric & IProduct Repository

---
+++ OnionArchitecture.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnionArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Application.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
