b200a351e96283c6263bf9204fb2cada8cdc5202
raihanM95 <mahmud.raihan@yahoo.com>
2021-04-18 11:35:11 +0600

    GenericRepository & ProductRepository Implemented

---
+++ OnionArchitecture.Infrastructure.Service/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnionArchitecture.Application.Interfaces;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Infrastructure.Service.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
